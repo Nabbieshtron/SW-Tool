9a1fc314b5fc799b5457060feca633869e385689
Nabbieshtron <56271010+Nabbieshtron@users.noreply.github.com>
2023-06-29 21:24:35 +0300

    Add files via upload

--- efficiency.py
+++ efficiency.py
@@ -1,15 +1,13 @@
-import pygame
+import constants
 from math import ceil
 
 
 class Efficiency:
     def __init__(self):
-        self.FONT = pygame.font.SysFont("Arial", 30, True)
         # User modified
         self.sub_stat_multipliers = {}
         self.synergy_multiplier = 0
-        self.roll_efficiency_default_value = True
-        self.rune_efficiency_inate = True
+        self.basic_value = True
         # List of information about rune
         self.rune_data = None
         # Rune roll efficiency str,int
@@ -18,58 +16,88 @@ class Efficiency:
         self.rune_eff = "N"
         # Information about rune grade
         self.grades = {}
-        # Multiplier
-        self.DEFAULT_EFFICIENCY_MULTIPLIER = 1
 
-        self.TEXT_COLORS = {
-            "magic": (223, 242, 135),
-            "rare": (185, 254, 255),
-            "hero": (255, 213, 244),
-            "legend": (255, 178, 72),
-        }
-        self.BG_COLORS = {
-            "magic": (30, 63, 6),
-            "rare": (7, 66, 74),
-            "hero": (92, 19, 65),
-            "legend": (115, 50, 20),
-        }
-
-        self.DEFAULT_GRADES = {
-            "magic": self.FONT.render("Magic", True, self.TEXT_COLORS["magic"]),
-            "rare": self.FONT.render("Rare", True, self.TEXT_COLORS["rare"]),
-            "hero": self.FONT.render("Hero", True, self.TEXT_COLORS["hero"]),
-            "legend": self.FONT.render("Legend", True, self.TEXT_COLORS["legend"]),
-        }
-        self.SUB_DEFAULT_VALUES = {
-            "cri rate": [4, 6],
-            "spd": [4, 6],
-            "cri dmg": [4, 7],
-            "atk": [5, 8],
-            "hp": [5, 8],
-            "def": [5, 8],
-            "resistance": [4, 8],
-            "accuracy": [4, 8],
-            "f_atk": [10, 20],
-            "f_def": [10, 20],
-            "f_hp": [135, 375],
+        # Default value of efficiency multiplier
+        self.sub_modifiers: dict = {
+            "no_flat": {
+                "cri rate": 1,
+                "cri dmg": 1,
+                "atk": 1,
+                "hp": 1,
+                "def": 1,
+                "resistance": 1,
+                "accuracy": 1,
+            },
+            "flat": {
+                "spd": 0.5,
+                "atk": 0.5,
+                "def": 0.5,
+                "hp": 0.5,
+            },
         }
 
     def rune_efficiency(self):
-        if self.ref:
-            # Get synergy
-            if list(self.ref.values()).count(max(self.ref.values())) > 1:
-                print("duplicate")
-            else:
-                print("not dupe")
-                print(self.rune_data["set"])
+        ratio = 0.0
+        state = "flat"
 
-            # Calculate multipliers
-            multiplier = 1.4
-            # Get total value of roll efficiency
-            total = sum(self.ref.values())
-            # Calculate efficiency
-            total_eff = total / 9 * multiplier
-            self.rune_eff = str(ceil(total_eff))
+        # Main
+        if len(self.rune_data["main"]) == 2:
+            name, value = self.rune_data["main"]
+            name = name.lower().strip()
+            value, state = (
+                (value[1:-1], "no_flat") if "%" in value else (value[1:], "flat")
+            )
+
+            if value.isdigit() and name in constants.DEFAULT_MAIN_POWER_UP[state]:
+                level = int(self.rune_data["level"][0])
+
+                # Formula: level * increment value + basic value == current value
+                basic, increment, highest = constants.DEFAULT_MAIN_POWER_UP[state][name]
+
+                if level == 15:
+                    percent = lambda part, whole: int(whole / 100 * part + whole)
+
+                    level -= 1
+                    six_star = percent(20, level * increment[1] + basic[1])
+                    five_star = percent(20, level * increment[0] + basic[1])
+                else:
+                    six_star = level * increment[1] + basic[1]
+                    five_star = level * increment[0] + basic[0]
+
+                # If True: 6 star grade
+                if six_star == int(value):
+                    ratio += 1.0
+
+                # If True: 5 star grade
+                elif five_star == int(value):
+                    ratio += highest[0] / highest[1]
+                else:
+                    print("Check rune efficincy method")
+
+                # Sub stat , inates
+                for name, value, grinds in self.rune_data["subs"]:
+                    name = name.lower().strip()
+                    value, state = (
+                        (value[1:-1], "no_flat")
+                        if "%" in value
+                        else (value[1:], "flat")
+                    )
+                    roll_values = constants.DEFAULT_SUB_VALUES[state]
+                    if value.isdigit() and name in roll_values:
+                        ratio += (
+                            int(value)
+                            / roll_values[name][1]
+                            * 0.2
+                            * self.sub_modifiers[state][name]
+                        )
+                    else:
+                        self.rune_eff = "N"
+                        break
+                # Calculate efficiency
+                total_eff = (ratio / 2.8) * 100
+                self.rune_eff = str(ceil(total_eff))
+            else:
+                self.rune_eff = "N"
         else:
             self.rune_eff = "N"
 
@@ -78,15 +106,19 @@ class Efficiency:
         try:
             for sub_name, sub_value in data:
                 sub_name, sub_value = sub_name.strip().lower(), sub_value.strip()
-                if not all(x in sub_value for x in ("+", "%")) and sub_name != "spd":
-                    sub_name = f"f_{sub_name}"
+                sub_value, state = (
+                    (int(sub_value[1:-1]), "no_flat")
+                    if "%" in sub_value
+                    else (int(sub_value[1:]), "flat")
+                )
 
-                sub_value = int(sub_value.replace("%", "").replace("+", ""))
                 value = 0
-                if sub_name in self.SUB_DEFAULT_VALUES:
+                if sub_name in constants.DEFAULT_SUB_VALUES[state]:
                     # Calculates efficiency value
-                    value = (sub_value / self.SUB_DEFAULT_VALUES[sub_name][1]) * 100
-                    if self.roll_efficiency_default_value is False:
+                    value = (
+                        sub_value / constants.DEFAULT_SUB_VALUES[state][sub_name][1]
+                    ) * 100
+                    if self.basic_value is False:
                         value -= 100
 
                     # checks if value negative
@@ -112,50 +144,54 @@ class Efficiency:
 
         # To calculate new efficiency
         new_data = []
-        # 0 - len(rune_data) to remove 4 base substats
-        total_rolls = 0 - len(self.rune_data["subs"])
+        # 0 - len(rune_data) to remove 4 base values of substat
+        total_rolls = 0 if self.basic_value else 0 - len(self.rune_data["subs"])
         highest_grade = None
         lowest_grade = None
+        flag = True
 
         for sub_name, sub_value, grind_value in self.rune_data["subs"]:
             sub_name = sub_name.lower().strip()
-            name = sub_name
-            if not all(x in sub_value for x in ("+", "%")) and name != "spd":
-                name = f"f_{name}"
+            sub_value, state = (
+                (sub_value[1:-1], "no_flat")
+                if "%" in sub_value
+                else (sub_value[1:], "flat")
+            )
+            if sub_value.isdigit():
+                sub_value = int(sub_value)
+                if sub_name in constants.DEFAULT_SUB_VALUES[state]:
+                    lowest_roll, highest_roll = constants.DEFAULT_SUB_VALUES[state][
+                        sub_name
+                    ]
+                    # Calculating roll number
+                    roll_num = ceil(sub_value / highest_roll)
 
-            try:
-                sub_value = int(sub_value.replace("%", "").replace("+", ""))
-            except ValueError:
-                sub_value = 1
-                # Calculating roll number
-            try:
-                roll_num = ceil(sub_value / self.SUB_DEFAULT_VALUES[name][1])
-            except KeyError:
-                roll_num = 1
-            # Modifying to get efficieny of lowest values
+                    # New list for lowest quality
+                    new_data.append([sub_name, f"+{str(roll_num * lowest_roll)}%"])
+                    total_rolls += roll_num
+                else:
+                    flag = False
+                    break
+            else:
+                flag = False
+        if flag:
+            # Highest estimated possible Grade
+            highest_grade = check_grade(total_rolls * 100)
+            # Lowest estimated possible Grade
+            total_rolls = sum(self.roll_efficiency(new_data).values())
+            lowest_grade = check_grade(total_rolls)
 
-            try:
-                v = [sub_name, f"+{str(roll_num * self.SUB_DEFAULT_VALUES[name][0])}%"]
-                if name in ("spd", "f_atk", "f_hp", "f_def"):
-                    v[1] = v[1].replace("%", "")
-                new_data.append(v)
-            except KeyError:
-                pass
-            total_rolls += roll_num
+            self.grades["el_grade"] = lowest_grade
+            self.grades["eh_grade"] = highest_grade
 
-        # Highest estimated possible Grade
-        highest_grade = check_grade(total_rolls * 100)
-        # Lowest estimated possible Grade
-        total_rolls = sum(self.roll_efficiency(new_data).values())
-        lowest_grade = check_grade(total_rolls)
-
-        self.grades["el_grade"] = lowest_grade
-        self.grades["eh_grade"] = highest_grade
-
-        # Adds all efficiency of substats
-        total = sum(self.ref.values())
-        # Checks the sub grade
-        self.grades["sub_grade"] = check_grade(total)
+            # Adds all efficiency of substats
+            total = sum(self.ref.values())
+            # Checks the sub grade
+            self.grades["sub_grade"] = check_grade(total)
+        else:
+            self.grades["el_grade"] = ""
+            self.grades["eh_grade"] = ""
+            self.grades["sub_grade"] = ""
 
     def update(self, data):
         if data:
